b77681d49e9d94a6ab3a0496e761f3b657304ef9
Rahul-1434 <157436820+Rahul-1434@users.noreply.github.com>
2025-03-14 10:52:20 +0530

    Update exp3b.ino

--- exp3b.ino
+++ exp3b.ino
@@ -1,36 +1,45 @@
+#include "DHT.h"
+#define DHTPIN 19
+#define DHTTYPE DHT11
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x3F, 16, 2); 
-
-#define trigPin 23
-#define echoPin 39
-
-long duration;
-int distance;
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.clear();
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
   Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.0344 / 2;
-  lcd.clear();
+  delay(2000);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(humidity) || isnan(temperature) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(humidity);
+  Serial.print(F("% Temperature: "));
+  Serial.print(temperature);
+  Serial.println(F("°C "));
+
   lcd.setCursor(0, 0);
-  lcd.print("Distance: ");
-  lcd.print(distance);
-  lcd.print(" cm");
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(500);  
+  lcd.print("Temp");
+  lcd.setCursor(5, 0);
+  lcd.print((char)223);
+  lcd.setCursor(6, 0);
+  lcd.print("C");
+  lcd.setCursor(9, 0);
+  lcd.print("Humi %");
+  lcd.setCursor(0, 1);
+  lcd.print(temperature);
+  lcd.setCursor(9, 1);
+  lcd.print(humidity);
 }
